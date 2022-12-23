185048263b9f7f84d8a6b51d0aaf50e2fb81a479
ArturMingl <adiscov90@gmail.com>
2022-12-23 15:00:46 +0300

    Zadacha54 Reshena

---
+++ Zadacha54/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void Fill2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j],3}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.WriteLine("Колличество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Колличество столбцов");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+int[,] table = new int[rows, colums];
+Fill2DArray(table);
+Print2DArray(table);
+Console.WriteLine();
+SortArray(table);
+Print2DArray(table);
